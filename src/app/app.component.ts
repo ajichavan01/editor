a355c1ccd306a317972b3d2b07132dec1bd31fbd
ajichavan01 <ajichavan01@gmail.com>
2025-12-15 20:34:35 -0600

    feat: add angular animations and animate left, right sidebar and terminal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskbarModule } from './taskbar/taskbar.module';
 import { LeftSidebarModule } from './left-sidebar/left-sidebar.module';
 import { RightSidebarModule } from './right-sidebar/right-sidebar.module';
 import { EditorModule } from './editor/editor.module';
 import { TerminalModule } from './terminal/terminal.module';
+import { EditorService } from './shared/editor.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskbarModule, LeftSidebarModule, RightSidebarModule, EditorModule, TerminalModule],
+  imports: [RouterOutlet, CommonModule, TaskbarModule, LeftSidebarModule, RightSidebarModule, EditorModule, TerminalModule],
+  providers: [EditorService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'editor';
+
+  constructor(public editorService: EditorService) {}
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/taskbar/taskbar/taskbar.component.ts
+++ src/app/taskbar/taskbar/taskbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EditorService } from '../../shared/editor.service';
 
 @Component({
   selector: 'app-taskbar',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './taskbar.component.scss'
 })
 export class TaskbarComponent {
+
+  constructor(private readonly editorService: EditorService) { }
+
+  toggleTerminal(): void {
+    this.editorService.toggleTerminal();
+  }
+
+  toggleLeftSidebar(): void {
+    this.editorService.toggleLeftSidebar();
+  }
+
+  toggleRightSidebar(): void {
+    this.editorService.toggleRightSidebar();
+  }
 
 }
